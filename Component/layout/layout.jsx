9d8e773126a1b99b4eeb2c3dbf9bc9e1c114da4f
Fatma Mourad <120746741+fatmamorad@users.noreply.github.com>
2025-07-26 20:13:48 +0300

    Update layout.jsx

--- Component/layout/layout.jsx
+++ Component/layout/layout.jsx
@@ -4,12 +4,11 @@ import { Outlet } from 'react-router-dom';
 import NavBar from '../News/NavBar.jsx';
 import Footer from '../footer/footer.jsx';
 
-// داخل الـ function فقط JSX
 function Layout() {
   return (
     <>
       <NavBar />
-          <Outlet /> {/* ده اللي بيعرض الصفحات الفرعية */}
+          <Outlet /> 
       <Footer />
     </>
   );
